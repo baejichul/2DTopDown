27782476b848d485393916d3e020f64d01ae6b63
baejichul <jayseebae@naver.com>
2022-07-04 22:39:04 +0900

    0704

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,28 +8,47 @@ public class GameManager : MonoBehaviour
     public TalkManager _talkManager;
     public QuestManager _questManager;
     public GameObject _canvas;
+    public GameObject _menuSet;
+    public GameObject _player;
     public Animator _dialog;
     public TypeEffect _talkText;
     public GameObject _scanObject;
+    public Text _questText;
     public bool _isAction;
     public int _talkIndex;
     public Image _portraitImg;
 
+
+    void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _canvas = GameObject.FindGameObjectWithTag("Canvas");
+        _menuSet = _canvas.transform.Find("MenuSet").gameObject;
+        _dialog = _canvas.transform.Find("Dialog").gameObject.GetComponent<Animator>();
+        _questText = _menuSet.transform.Find("QuestPanel").Find("TxtQuest").GetComponent<Text>();
+        // _talkText = _canvas.transform.Find("Dialog").Find("Image").Find("Text").GetComponent<Text>();
+        _talkText = _canvas.transform.Find("Dialog").Find("Image").Find("Text").GetComponent<TypeEffect>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        _canvas   = GameObject.FindGameObjectWithTag("Canvas");
-        _dialog   = _canvas.transform.Find("Dialog").gameObject.GetComponent<Animator>();
-        // _talkText = _canvas.transform.Find("Dialog").Find("Image").Find("Text").GetComponent<Text>();
-        _talkText = _canvas.transform.Find("Dialog").Find("Image").Find("Text").GetComponent<TypeEffect>();
-
+        GameLoad();
         // Debug.Log ( _questManager.CheckQuest() );
+        _questText.text = _questManager.CheckQuest();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Sub Menu
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (_menuSet.activeSelf)
+                _menuSet.SetActive(false);
+            else
+                _menuSet.SetActive(true);
+        }   
     }
 
     public void Action(GameObject scanObj)
@@ -62,7 +81,8 @@ public class GameManager : MonoBehaviour
         {
             _isAction = false;
             _talkIndex = 0;
-            Debug.Log( _questManager.CheckQuest(id) );
+            // Debug.Log( _questManager.CheckQuest(id) );
+            _questText.text = _questManager.CheckQuest(id);
             return;
         }
             
@@ -83,5 +103,40 @@ public class GameManager : MonoBehaviour
 
         _isAction = true;
         _talkIndex++;
+    }
+
+    public void GameSave()
+    {
+        // _player.x
+        // _player.y
+        // _questId
+        // _questActionIndex
+        PlayerPrefs.SetFloat("PlayerX", _player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", _player.transform.position.y);
+        PlayerPrefs.SetInt("QuestId", _questManager._questId);
+        PlayerPrefs.SetInt("QuestActionIndex", _questManager._questActionIndex);
+        PlayerPrefs.Save();
+
+        _menuSet.SetActive(false);
+    }
+
+    public void GameLoad()
+    {
+        if ( !PlayerPrefs.HasKey("PlayerX") )
+            return;
+
+        float x = PlayerPrefs.GetFloat("PlayerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+        int questId = PlayerPrefs.GetInt("QuestId");
+        int questActionIndex = PlayerPrefs.GetInt("QuestActionIndex");
+
+        _player.transform.position = new Vector3(x, y);
+        _questManager._questId = questId;
+        _questManager._questActionIndex = questActionIndex;
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
     }
 }
